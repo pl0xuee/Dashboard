357da3bde6d0e847adf5f4b7c8dd2162fd21f501
Your Name <you@example.com>
2026-06-20 00:35:07 -0700

    Automated backup 2026-06-20-00-35-07 Summary of changes:  config.js         |   1 +  stream/index.html | 275 ++++++++++++++++++++++++++++++++++++++++++++++++------  2 files changed, 247 insertions(+), 29 deletions(-)

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const API_KEY = 'YOUR_API_KEY';
 export const TWITCH_CLIENT_ID = 'fjsw1bjffiqyraujar8p297ltorexf';
+export const YOUTUBE_CLIENT_ID = '743961588451-ba0t0lm7nd7m3nq10042fan3a8djmnqo.apps.googleusercontent.com';
